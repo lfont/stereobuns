1b25b90425f4a2e5211584d6ce58bab60a15d14a
Loïc Fontaine <ljph.fontaine@gmail.com>
2013-06-23 13:23:53 +0200

    Added a directive to prevent scroll propagation from the queue modal.

--- public/js/directives/main.js
+++ public/js/directives/main.js
@@ -8,13 +8,16 @@ define([
     './artwork-drt',
     './song-drt',
     './song-bar-drt',
+    './no-scroll-propagation-drt',
     'controllers'
 ], function (angular, ArtworkDrtFactory,
-            SongDrtFactory, SongBarDrtFactory) {
+             SongDrtFactory, SongBarDrtFactory,
+             noScrollPropagationDrtFactory) {
     'use strict';
     
     return angular.module('soundrocket.directives', [ 'soundrocket.controllers' ])
                   .directive('artwork', ArtworkDrtFactory)
                   .directive('song', SongDrtFactory)
-                  .directive('songbar', SongBarDrtFactory);
+                  .directive('songbar', SongBarDrtFactory)
+                  .directive('noScrollPropagation', noScrollPropagationDrtFactory);
 });
