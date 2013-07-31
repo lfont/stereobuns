e6870dd03ba06ba9226586be2f087dfaf58d2929
Loïc Fontaine <ljph.fontaine@gmail.com>
2013-07-31 19:05:17 +0200

    Added shortcut management.

--- public/js/directives/main.js
+++ public/js/directives/main.js
@@ -4,15 +4,18 @@ Loïc Fontaine - http://github.com/lfont - MIT Licensed
 */
 
 define([
-    'angular',
-    './artwork-drt',
-    './no-scroll-propagation-drt'
+  'angular',
+  './artwork-drt',
+  './no-scroll-propagation-drt',
+  './shortcut-drt'
 ], function (angular,
              artworkDrtFactory,
-             noScrollPropagationDrtFactory) {
-    'use strict';
-    
-    return angular.module('soundrocket.directives', [])
-                  .directive('artwork', artworkDrtFactory)
-                  .directive('noScrollPropagation', noScrollPropagationDrtFactory);
+             noScrollPropagationDrtFactory,
+             shortcutDrtFactory) {
+  'use strict';
+
+  return angular.module('soundrocket.directives', [])
+                .directive('artwork', artworkDrtFactory)
+                .directive('noScrollPropagation', noScrollPropagationDrtFactory)
+                .directive('shortcut', shortcutDrtFactory);
 });
