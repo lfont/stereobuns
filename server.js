1b0c649f9402a8e553e34f78a8d2e47992efc0c0
Loïc Fontaine <ljph.fontaine@gmail.com>
2013-08-08 14:20:39 +0200

    Cleaning the tomahawk lib. Added the 8tracks resolver.

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ A sound aggregator.
 Loïc Fontaine - http://github.com/lfont - MIT Licensed
 */
 
-var http         = require('http'),
-    app          = require('./app'),
-    searchServer = require('./lib/search-server'),
-    config       = require('./lib/configuration');
+var http     = require('http'),
+    app      = require('./app'),
+    tomahawk = require('./lib/tomahawk'),
+    config   = require('./lib/configuration');
 
 var server = http.createServer(app);
 
-searchServer.listen(server, app);
+tomahawk.listen(server, app);
 
 server.listen(config.serverPort, function () {
-    console.log('Express server listening on port %d in %s mode',
-                config.serverPort, app.settings.env);
+  console.log('Express server listening on port %d in %s mode',
+              config.serverPort, app.settings.env);
 });
