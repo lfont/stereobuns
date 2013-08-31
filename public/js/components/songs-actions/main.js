b1f579092ac50472abe3c19d9da84ba7115ac9d1
Loïc Fontaine <ljph.fontaine@gmail.com>
2013-08-31 17:13:39 -0400

    Fix broken directive rendering (css class not applied).

--- public/js/components/songs-actions/main.js
+++ public/js/components/songs-actions/main.js
@@ -4,25 +4,25 @@ Loïc Fontaine - http://github.com/lfont - MIT Licensed
 */
 
 define([
-    'angular',
-    './songs-actions-drt-ctrl',
-    './songs-actions-drt',
-    'services',
-    'models',
-    'components/playlist-chooser'
+  'angular',
+  './songs-actions-drt-ctrl',
+  './songs-actions-drt',
+  'services',
+  'models',
+  'components/playlist-chooser'
 ], function (angular,
              SongsActionsDrtCtrl,
              songsActionsDrtFactory) {
-    'use strict';
-    
-    var songbarComponent = angular.module('soundrocket.components.songs-actions', [
-        'soundrocket.services',
-        'soundrocket.models',
-        'soundrocket.components.playlist-chooser'
-    ]);
-    
-    songbarComponent.controller('SongsActionsDrtCtrl', SongsActionsDrtCtrl)
-                    .directive('songsActions', songsActionsDrtFactory);
-    
-    return songbarComponent;
+  'use strict';
+  
+  var songbarComponent = angular.module('soundrocket.components.songs-actions', [
+    'soundrocket.services',
+    'soundrocket.models',
+    'soundrocket.components.playlist-chooser'
+  ]);
+  
+  songbarComponent.controller('SongsActionsDrtCtrl', SongsActionsDrtCtrl)
+                  .directive('songsActions', songsActionsDrtFactory);
+  
+  return songbarComponent;
 });
